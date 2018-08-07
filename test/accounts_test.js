ce0fd335a6258eadbe84f24253837105f8d3860a
kjunu <junwooo.kim@gmail.com>
2018-08-07 16:33:54 +0900

    Fix account test case

--- test/accounts_test.js
+++ test/accounts_test.js
@@ -8,7 +8,7 @@ import Aergo from '../src';
 
 describe('Aergo.Accounts', () => {
     const aergo = new Aergo(); //default connect to 127.0.0.1:7845
-    let testAddress = '';
+    let testAddress = '2a4b7eNT4nX5d76RbT6tq9u4PvNx';
     describe('create()', () => {
         it('should return created base58 encoded address', (done) => {
             aergo.accounts.create('testpass').then((address) => {
@@ -47,31 +47,37 @@ describe('Aergo.Accounts', () => {
     });
 
     describe('signTX()', () => {
-        it('should return tx which has a unlocked account sign', (done) => {
+        it('unlocked account for sign', (done) => {
             aergo.accounts.unlock(testAddress, 'testpass').then((address) => {
                 assert.isString(address);
-                const testtx = {
-                    nonce: 1,
-                    from: address,
-                    to: address,
-                    amount: 123,
-                    payload: null,
-                };
-                aergo.accounts.signTransaction(testtx).then((result) => {
+                done();
+            });
+        });
+
+        it('should return tx which has a unlocked account sign', (done) => {
+            const testtx = {
+                nonce: 1,
+                from: testAddress,
+                to: testAddress,
+                amount: 123,
+                payload: null,
+            };
+            aergo.accounts.signTransaction(testtx)
+                .then((result) => {
                     assert.equal(testtx.nonce, result.nonce);
                     assert.equal(testtx.from, result.from);
                     assert.typeOf(result.sign, 'Uint8Array');
                     assert.equal(result.sign.length, 65);
                     done();
                 });
-            });
         });
     });
 
     describe('sendTransaction()', () => {
         it('should send signed transaction', async () => {
-            const address = await aergo.accounts.unlock(testAddress, 'testpass');
-            assert.equal(address, testAddress);
+            const createdAddress = await aergo.accounts.create('testpass');
+            const address = await aergo.accounts.unlock(createdAddress, 'testpass');
+            assert.equal(address, createdAddress);
             const testtx = {
                 nonce: 1,
                 from: address,
@@ -86,17 +92,18 @@ describe('Aergo.Accounts', () => {
             assert.equal(txhash.length, 32);
             // Submitting same tx again should error
             return assert.isRejected(aergo.sendTransaction(tx));
-        }).timeout(5000);
+        });
     });
 
     describe('signTX(),sendTX()Perf', () => {
-        it('should return tx which has a unlocked account sign', async () => {
-            await aergo.accounts.unlock(testAddress, 'testpass');
-            for (let i = 2; i <= 1000; i++) {
+        it('should not timeout, synchronous sign-send', async () => {
+            const createdAddress = await aergo.accounts.create('testpass');
+            const address = await aergo.accounts.unlock(createdAddress, 'testpass');
+            for (let i = 1; i <= 1000; i++) {
                 const testtx = {
                     nonce: i,
-                    from: testAddress,
-                    to: testAddress,
+                    from: address,
+                    to: address,
                     amount: i,
                     payload: null,
                 };
