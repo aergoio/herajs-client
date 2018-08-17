4eda9e88f4c4e4980196ce945a68ebb3e139e8ac
Paul Grau <graup@users.noreply.github.com>
2018-08-17 14:15:56 +0900

    Implemented getTransaction, refactored some code (#3)

--- src/client/index.js
+++ src/client/index.js
@@ -1,10 +1,8 @@
-import rpcTypes from './types.js';
-import { fromHexString, fromNumber } from '../utils.js';
-import promisify from '../promisify.js';
-
-import Base58 from 'base-58';
-
 import Accounts from '../accounts';
+import rpcTypes from './types.js';
+import { toHexString, fromHexString, fromNumber } from '../utils.js';
+import promisify from '../promisify.js';
+import { transactionToTx, txToTransaction } from '../transactions/utils.js';
 
 const CommitStatus = rpcTypes.CommitStatus;
 export { CommitStatus };
@@ -13,7 +11,7 @@ class AergoClient {
     constructor (config) {
         this.version = 0.1;
         this.config = {
-            url: '127.0.0.1:7845', // protocol is determined by client
+            url: '127.0.0.1:7845', // Don't put http:// here, protocol is determined by client
             ...config
         };
         this.client = this.initClient(this.config);
@@ -21,7 +19,7 @@ class AergoClient {
     }
 
     initClient(config) {
-        // override
+        // platform-specific override
     }
 
     getConfig () {
@@ -37,9 +35,10 @@ class AergoClient {
         return promisify(this.client.blockchain, this.client)(empty);
     }
 
-    getTransaction () {
+    getTransaction (txid) {
         const singleBytes = new rpcTypes.SingleBytes();
-        return promisify(this.client.getTX, this.client)(singleBytes);
+        singleBytes.setValue(fromHexString(txid));
+        return promisify(this.client.getTX, this.client)(singleBytes).then(txToTransaction);
     }
 
     getBlock (hashOrNumber) {
@@ -63,7 +62,7 @@ class AergoClient {
         return promisify(this.client.verifyTX, this.client)(transactionToTx(tx));
     }
 
-    sendTransaction (tx) {        
+    sendTransaction (tx) {
         return new Promise((resolve, reject) => {
             const txs = new rpcTypes.TxList();
             txs.addTxs(transactionToTx(tx), 0);
@@ -75,7 +74,7 @@ class AergoClient {
                 if (err) {
                     reject(err);
                 } else {
-                    resolve(result.getResultsList()[0].getHash());
+                    resolve(toHexString(result.getResultsList()[0].getHash()));
                 }
             });
         });
@@ -88,21 +87,6 @@ class AergoClient {
 
 
 
-function transactionToTx(tx) {
-    const msgtxbody = new rpcTypes.TxBody();
-    msgtxbody.setNonce(tx.nonce);
-    msgtxbody.setAccount(Base58.decode(tx.from));
-    msgtxbody.setRecipient(Base58.decode(tx.to));
-    msgtxbody.setAmount(tx.amount);
-    if (tx.payload != null) {
-        msgtxbody.setPayload(tx.payload);
-    }
-    msgtxbody.setSign(tx.sign);
-    const msgtx = new rpcTypes.Tx();
-    msgtx.setHash(tx.hash);
-    msgtx.setBody(msgtxbody);
 
-    return msgtx;
-}
 
 export default AergoClient;
